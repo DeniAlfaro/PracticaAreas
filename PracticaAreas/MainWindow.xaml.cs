3268efce8ceafcf1015ef7df83d30b9447b4207e
Deni Alfaro <denilorenaalfaro@hotmail.com>
2018-09-27 08:49:53 -0700

    Actualizado

--- PracticaAreas/MainWindow.xaml.cs
+++ PracticaAreas/MainWindow.xaml.cs
@@ -25,42 +25,79 @@ namespace PracticaAreas
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            float baser = float.Parse(base1.Text);
+            /*float baser = float.Parse(base1.Text);
             float alturar = float.Parse(altura1.Text);
             float areatotal = baser * alturar;
-            area1.Text = areatotal.ToString();
+            area1.Text = areatotal.ToString();*/
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            float baset = float.Parse(base2.Text);
+            /*float baset = float.Parse(base2.Text);
             float alturat = float.Parse(altura2.Text);
             float areatotal2 = (baset * alturat) / 2;
-            area2.Text = areatotal2.ToString();
+            area2.Text = areatotal2.ToString();*/
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            float radioc = float.Parse(radio.Text);
+            /*float radioc = float.Parse(radio.Text);
             float PI = 3.1416f;
             float areatotal3 = PI * (radioc * radioc);
-            area3.Text = areatotal3.ToString();
+            area3.Text = areatotal3.ToString();*/
         }
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
-            float alturatr = float.Parse(altura3.Text);
+            /*float alturatr = float.Parse(altura3.Text);
             float basemayortr = float.Parse(basemayor.Text);
             float basemenortr = float.Parse(basemenor.Text);
             float areatotal4 = alturatr * ((basemayortr + basemenortr) / 2);
-            area4.Text = areatotal4.ToString();
+            area4.Text = areatotal4.ToString();*/
+        }
+
+        private void cbTipoFigura_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            panelConfiguracion.Children.Clear();
+            switch (cbTipoFigura.SelectedIndex)
+            {
+                case 0: //rectangulo
+                    panelConfiguracion.Children.Add(new ControlAreaRectangulo());
+                    break;
+                case 1: //triangulo
+                    panelConfiguracion.Children.Add(new ControlAreaTriangulo());
+                    break;
+                case 2: //circulo
+                    panelConfiguracion.Children.Add(new ControlAreaCirculo());
+                    break;
+                case 3: //trapecio
+                    panelConfiguracion.Children.Add(new ControlAreaTrapecio());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        double area = 0.0;
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            switch (cbTipoFigura.SelectedIndex)
+            {
+                case 0: //cuadrado
+                    break;
+                case 1: //triangulo
+                    break;
+                case 2: //circulo
+                    break;
+                case 3: //trapecio
+                    break;
+                default:
+                    break;
+            }
+            lblResultadoArea.Text = area.ToString();
         }
     }
 }
